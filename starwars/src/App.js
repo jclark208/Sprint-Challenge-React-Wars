a5ae1a2e3ba5c38a59a597567fcb8545b059090f
Jake Clark <jclark208@yahoo.com>
2020-02-21 13:20:29 -0500

    oh it is super fugly

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './App.css';
+import Characters from "./components/CharList"
+import styled from "styled-components";
+
+const AnnyoyingHeader = styled.h1`
+color: #FFFFFF;
+font-size: 72px;
+text-shadow: #FFF 0px 0px 5px, #FFF 0px 0px 10px, #FFF 0px 0px 15px, #FF2D95 0px 0px 20px, #FF2D95 0px 0px 30px, #FF2D95 0px 0px 40px, #FF2D95 0px 0px 50px, #FF2D95 0px 0px 75px;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +19,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <AnnyoyingHeader className="Header">React Wars</AnnyoyingHeader>
+      <Characters/>
     </div>
   );
 }
